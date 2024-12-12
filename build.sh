fc33dca0c712657aa505099486cd273d6f6fbc29
TronickDev <166713136+TronickDev@users.noreply.github.com>
2024-12-12 21:24:57 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,11 @@
 
 set -e
 
-`xcrun --sdk iphoneos -f clang` -dynamiclib -x objective-c -arch arm64 -isysroot `xcrun --sdk iphoneos --show-sdk-path` -framework Foundation -framework UIKit -miphoneos-version-min=13.0 -o LuckySpeeder.dylib LuckySpeeder.m -O3 -flto
+# compile for iphone arm64
+`xcrun --sdk iphoneos -f clang` -dynamiclib -x objective-c -arch arm64 -isysroot `xcrun --sdk iphoneos --show-sdk-path` -framework Foundation -framework UIKit -miphoneos-version-min=13.0 -o LuckySpeeder-iphone-arm64.dylib LuckySpeeder.m -O3 -flto
+
+# compile for macos arm64
+`xcrun --sdk iphoneos -f clang` -dynamiclib -x objective-c -arch arm64 -isysroot `xcrun --sdk iphoneos --show-sdk-path` -framework Foundation -framework UIKit -miphoneos-version-min=13.0 -o LuckySpeeder-macos-arm64.dylib LuckySpeeder.m -O3 -flto
+
+# compile for macos x86
+`xcrun --sdk iphoneos -f clang` -dynamiclib -x objective-c -arch arm64 -isysroot `xcrun --sdk iphoneos --show-sdk-path` -framework Foundation -framework UIKit -miphoneos-version-min=13.0 -o LuckySpeeder-macos-x86.dylib LuckySpeeder.m -O3 -flto
